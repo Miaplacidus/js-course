2a19d1d3cf4acb452b87c8a5afe6856ba4328ace
Ifu Aniemeka <ianiemeka@gmail.com>
2014-03-31 11:30:27 -0500

    Complete functions to spec

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,22 @@
 (function () {
 
-  window.Car = function () {
+  window.Car = function (color, person) {
     // TODO
+    this.state = "off" ;
+    this.color = color;
+    this.driver = person;
+    this.ignite = function(){
+      if (this.driver && this.driver.hasDriversLicense == true){
+      this.state = 'running';
+      return "Vroom!";
+      }
+      else if (!this.driver) {
+        return "There is no driver to start the car!"
+      }
+      else {
+        return "Sorry, you do not have a license."
+      }
+    };
   };
 
 })();
--- src/Person.js
+++ src/Person.js
@@ -2,6 +2,11 @@
 
   window.Person = function (name) {
     // TODO
+    this.name = name;
+    this.hasDriversLicense = false;
+    this.receiveLicense = function(){
+      this.hasDriversLicense = true;
+    }
   };
 
 })();
